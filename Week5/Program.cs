83ada82f98a7b6e3501ac48af28c08ba760b5bd8
Anna D <denysenko.niura@gmail.com>
2023-05-09 18:50:33 +0300

    week5

---
+++ Week5/Program.cs
@@ -0,0 +1,34 @@
+﻿//AND (&&) and OR (||) logical operators
+
+using System.ComponentModel.Design;
+using System.Transactions;
+
+string username, password; // username: admin // password 12345678
+
+Console.WriteLine("Enter username:");
+username = Console.ReadLine();
+
+Console.WriteLine("Enter the password:");
+password = Console.ReadLine();
+
+
+// OR             // != NOT
+if (username != "admin" || password != "12345678")
+{
+    Console.WriteLine("Invalid user credentials");
+}
+else
+{
+    Console.WriteLine("Welcome");
+}
+
+
+// AND
+//if (username == "admin" && password == "12345678") 
+//{
+//    Console.WriteLine("Welcome");
+//}
+//else
+//{
+//    Console.WriteLine("Invalid user credentials");
+//}
